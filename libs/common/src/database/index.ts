0ace884327f940c06e3bf33162db0a2b95154a2a
Arnav-Purushotam <arpu9852@colorado.edu>
2024-09-07 17:25:45 -0600

    second-commit-before-config-errors

---
+++ libs/common/src/database/index.ts
@@ -0,0 +1,3 @@
+export * from './database.module';
+//whenever there is a red signal on index.ts file name (where it shows as untracked
+//on hovering over it), just delete that index.ts file and create a new one
---
+++ libs/common/src/index.ts
@@ -0,0 +1,7 @@
+export * from './database';
+//when an error comes when exporting certain folders like database,
+//and everything seems current, just backspace that export and retype it
+
+//index.ts in the database folder exports from there to the src folder level
+//this index.ts meanwhile takes that export and exports it to the common level which
+//is accessible in the tsconfig.json
