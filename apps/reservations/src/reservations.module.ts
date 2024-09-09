d4b470ef7cbaa2e603ad0ace046f2d45f9879fc4
Arnav-Purushotam <arpu9852@colorado.edu>
2024-09-09 17:11:18 -0600

    commit-6 Validation_and_Logging

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -3,11 +3,23 @@ import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
-import { ReservationDocument, ReservationSchema } from './reservations/models/reservation.schema';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './reservations/models/reservation.schema';
+import { LoggerModule } from '@app/common/logger';
+
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}])],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+    LoggerModule
+    
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService,ReservationsRepository],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
